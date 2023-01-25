fcc82b677c0beb6c4729fd36779f8fb4e2ac79c5
roman <roman.rubinshtein@1win.pro>
2023-01-25 16:13:11 +0100

    first commit

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export const {
-  MYSQL_USER,
-  MYSQL_DATABASE,
-  MYSQL_ROOT_PASSWORD,
-  MYSQL_HOST,
-  MYSQL_PORT,
-} = process.env;
-
-export const { APIID, APIHASH, BOTID } = process.env;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,111 @@
-import 'dotenv/config';
-import { NewMessage } from 'telegram/events/index';
-import client from './clientAuth';
-import eventPrint from './eventPrint';
+import TelegramBot from 'node-telegram-bot-api';
 
-async function autoReply() {
-  try {
-    client.addEventHandler(eventPrint, new NewMessage({}));
-  } catch (error) {
-    console.log(error);
-  }
-}
+const bot = new TelegramBot('5985047743:AAH4vJy5RX9NBUmawsyxbhnIpCRoeHTjDmk', {
+  polling: {
+    params: {
+      allowed_updates: true,
+    },
+  },
+});
 
-autoReply();
+bot.on('message', async (msg) => {
+      if (msg.text) {
+        try {
+          const keyboard = [];
+          const textFromManager = msg.text.split(' ');
+          const number = textFromManager.findIndex((item) => item === 'кнопки:');
+          const buttons = textFromManager.slice(number + 1);
+          textFromManager.splice(number);
+          buttons.forEach((item) => {
+            const arr = item.split(',');
+            const buttonText = arr[0];
+            const link = arr[1];
+            keyboard.push([{
+              text: buttonText,
+              url: link,
+            }]);
+          });
+          await bot.sendMessage(
+            msg.from.id,
+            textFromManager.join(' '),
+            {
+              parse_mode: 'HTML',
+              reply_markup: JSON.stringify({
+                inline_keyboard:
+                keyboard,
+              }),
+            },
+          );
+        } catch (err) {
+          await bot.sendMessage(msg.from.id, 'text message wrong');
+          console.log(err.message);
+        }
+      }
+      if (msg.photo) {
+        try {
+          const keyboard = [];
+          const textFromManager = msg.caption.split(' ');
+          const number = textFromManager.findIndex((item) => item === 'кнопки:');
+          const buttons = textFromManager.slice(number + 1);
+          textFromManager.splice(number);
+          buttons.forEach((item) => {
+            const arr = item.split(',');
+            const buttonText = arr[0];
+            const link = arr[1];
+            keyboard.push([{
+              text: buttonText,
+              url: link,
+            }]);
+          });
+          const photo = msg.photo.find((item) => item.file_id);
+          await bot.sendPhoto(msg.from.id, photo.file_id, {
+            parse_mode: 'HTML',
+            caption: textFromManager.join(' '),
+            reply_markup: JSON.stringify({
+              inline_keyboard:
+                  keyboard,
+            }),
+          });
+        } catch (err) {
+          await bot.sendMessage(msg.from.id, 'photo with text message wrong');
+          console.log(err.message);
+        }
+      }
+      if (msg.video) {
+        try {
+          const keyboard = [];
+          const textFromManager = msg.caption.split(' ');
+          const number = textFromManager.findIndex((item) => item === 'кнопки:');
+          const buttons = textFromManager.slice(number + 1);
+          textFromManager.splice(number);
+          buttons.forEach((item) => {
+            const arr = item.split(',');
+            const buttonText = arr[0];
+            const link = arr[1];
+            keyboard.push([{
+              text: buttonText,
+              url: link,
+            }]);
+          });
+          await bot.sendVideo(msg.from.id, msg.video.file_id, {
+            parse_mode: 'HTML',
+            caption: textFromManager.join(' '),
+            reply_markup: JSON.stringify({
+              inline_keyboard:
+                  keyboard,
+            }),
+          });
+        } catch (err) {
+          await bot.sendMessage(msg.from.id, 'video with text message wrong');
+          console.log(err.message);
+        }
+      }
+      if (msg.video_note) {
+        try {
+          await bot.sendVideoNote(msg.from.id, msg.video_note.file_id);
+        } catch (err) {
+          await bot.sendMessage(msg.from.id, 'video note message wrong');
+          console.log(err.message);
+        }
+      }
+});
